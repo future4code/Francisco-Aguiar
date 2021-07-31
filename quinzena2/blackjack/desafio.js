12efc0c6f790f7805595478426b4600691aa2942
Francisco Hugo de Azevedo Aguiar <hugo-phone@live.com>
2021-07-31 10:24:14 -0300

    Desafio do Projeto Blackjack em andamento

--- quinzena2/blackjack/desafio.js
+++ quinzena2/blackjack/desafio.js
@@ -11,3 +11,65 @@
  * 
  */
 
+    console.log ("Bem vindo(a) ao jogo de Blackjack!")
+
+
+    if (confirm ("Quer iniciar uma nova rodada?")) {
+       let primeiraCartaUsuario = comprarCarta()
+       let segundaCartaUsuario = comprarCarta()
+       let primeiraCartaComputador = comprarCarta()
+       let segundaCartaComputador = comprarCarta()
+    
+       let valorCartasUsuario = primeiraCartaUsuario.valor + segundaCartaUsuario.valor
+       let valorCartasComputador = primeiraCartaComputador.valor + segundaCartaComputador.valor
+    
+       console.log (`Usuário - cartas: ${primeiraCartaUsuario.texto} ${segundaCartaUsuario.texto}  - pontuação ${valorCartasUsuario}`)
+       console.log (`Computador - cartas: ${primeiraCartaComputador.texto}`)
+
+    
+       if ((primeiraCartaUsuario.texto.includes("A") && segundaCartaUsuario.texto.includes("A")) || (primeiraCartaComputador.texto.includes("A") && segundaCartaComputador.texto.includes("A"))){
+          console.log("Jogador com Dois 'As'. As cartas serão sorteadas novamente.")
+
+
+          primeiraCartaUsuario = comprarCarta()
+          segundaCartaUsuario = comprarCarta()
+          primeiraCartaComputador = comprarCarta()
+          segundaCartaComputador = comprarCarta()
+
+          valorCartasUsuario = primeiraCartaUsuario.valor + segundaCartaUsuario.valor
+          valorCartasComputador = primeiraCartaComputador.valor + segundaCartaComputador.valor
+    
+          
+          console.log (`Usuário - cartas: ${primeiraCartaUsuario.texto} ${segundaCartaUsuario.texto}  - pontuação ${valorCartasUsuario}`)
+          console.log (`Computador - cartas: ${primeiraCartaComputador.texto}`)
+              
+          if (valorCartasUsuario > valorCartasComputador){
+            console.log ("O usuário ganhou!")
+         }
+          else if (valorCartasUsuario < valorCartasComputador){
+            console.log ("O computador ganhou!")
+         }
+          else {
+            console.log ("Empate")
+         }
+       }
+
+       else {
+         if (valorCartasUsuario > valorCartasComputador){
+            console.log ("O usuário ganhou!")
+         }
+         else if (valorCartasUsuario < valorCartasComputador){
+            console.log ("O computador ganhou!")
+         }
+         else {
+            console.log ("Empate")
+         }
+          
+       }
+
+
+       
+    }
+    else {
+       console.log ("O jogo acabou.")
+    }
