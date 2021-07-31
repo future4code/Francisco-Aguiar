844d10752edb4744385a80a5e0ab33e85f05236c
Francisco Hugo de Azevedo Aguiar <hugo-phone@live.com>
2021-07-31 14:10:00 -0300

    Blackjack, desafio em desenvolvimento

--- quinzena2/blackjack/desafio.js
+++ quinzena2/blackjack/desafio.js
@@ -12,64 +12,68 @@
  */
 
     console.log ("Bem vindo(a) ao jogo de Blackjack!")
+    let primeiraCartaUsuario = comprarCarta()
+    let segundaCartaUsuario = comprarCarta()
+    let primeiraCartaComputador = comprarCarta()
+    let segundaCartaComputador = comprarCarta()
+   
+    let cartasDoUsuario = [primeiraCartaUsuario, segundaCartaUsuario]
+    let cartasDoComputador = [primeiraCartaComputador, segundaCartaComputador]
 
+    let valorCartasUsuario = primeiraCartaUsuario.valor + segundaCartaUsuario.valor
+    let valorCartasComputador = primeiraCartaComputador.valor + segundaCartaComputador.valor
+
+      
+    const resultado = (valorUsuario, valorComputador) => {
+      if (valorUsuario > valorComputador){
+         console.log ("O usuário ganhou!")
+      }
+      else if (valorUsuario < valorComputador){
+         console.log ("O computador ganhou!")
+      }
+      else {
+         console.log ("Empate")
+      }
+    }
+
+    const comprarNovaCarta = () => {
+      const novaCarta = comprarCarta() 
+      return novaCarta
+    }
 
     if (confirm ("Quer iniciar uma nova rodada?")) {
-       let primeiraCartaUsuario = comprarCarta()
-       let segundaCartaUsuario = comprarCarta()
-       let primeiraCartaComputador = comprarCarta()
-       let segundaCartaComputador = comprarCarta()
-    
-       let valorCartasUsuario = primeiraCartaUsuario.valor + segundaCartaUsuario.valor
-       let valorCartasComputador = primeiraCartaComputador.valor + segundaCartaComputador.valor
-    
-       console.log (`Usuário - cartas: ${primeiraCartaUsuario.texto} ${segundaCartaUsuario.texto}  - pontuação ${valorCartasUsuario}`)
-       console.log (`Computador - cartas: ${primeiraCartaComputador.texto}`)
+      console.log (`Usuário - cartas: ${primeiraCartaUsuario.texto} ${segundaCartaUsuario.texto}  - pontuação ${valorCartasUsuario}`)
+      console.log (`Computador - cartas: ${primeiraCartaComputador.texto}`)
 
-    
-       if ((primeiraCartaUsuario.texto.includes("A") && segundaCartaUsuario.texto.includes("A")) || (primeiraCartaComputador.texto.includes("A") && segundaCartaComputador.texto.includes("A"))){
-          console.log("Jogador com Dois 'As'. As cartas serão sorteadas novamente.")
+      while ((primeiraCartaUsuario.texto.includes("A") && segundaCartaUsuario.texto.includes("A")) || (primeiraCartaComputador.texto.includes("A") && segundaCartaComputador.texto.includes("A"))) {
+         console.log ("Duas cartas 'As' para o mesmo jogador. As cartas serão redistribuidas.");
+         primeiraCartaUsuario = comprarCarta()
+         segundaCartaUsuario = comprarCarta()
+         primeiraCartaComputador = comprarCarta()
+         segundaCartaComputador = comprarCarta()
+      
+         cartasDoUsuario = [primeiraCartaUsuario, segundaCartaUsuario]
+         cartasDoComputador = [primeiraCartaComputador, segundaCartaComputador]
+  
+         valorCartasUsuario = primeiraCartaUsuario.valor + segundaCartaUsuario.valor
+         valorCartasComputador = primeiraCartaComputador.valor + segundaCartaComputador.valor
 
+         console.log (`Usuário - cartas: ${primeiraCartaUsuario.texto} ${segundaCartaUsuario.texto}  - pontuação ${valorCartasUsuario}`);
+         console.log (`Computador - cartas: ${primeiraCartaComputador.texto}`);
 
-          primeiraCartaUsuario = comprarCarta()
-          segundaCartaUsuario = comprarCarta()
-          primeiraCartaComputador = comprarCarta()
-          segundaCartaComputador = comprarCarta()
+      }
 
-          valorCartasUsuario = primeiraCartaUsuario.valor + segundaCartaUsuario.valor
-          valorCartasComputador = primeiraCartaComputador.valor + segundaCartaComputador.valor
-    
-          
-          console.log (`Usuário - cartas: ${primeiraCartaUsuario.texto} ${segundaCartaUsuario.texto}  - pontuação ${valorCartasUsuario}`)
-          console.log (`Computador - cartas: ${primeiraCartaComputador.texto}`)
-              
-          if (valorCartasUsuario > valorCartasComputador){
-            console.log ("O usuário ganhou!")
-         }
-          else if (valorCartasUsuario < valorCartasComputador){
-            console.log ("O computador ganhou!")
-         }
-          else {
-            console.log ("Empate")
-         }
-       }
-
-       else {
-         if (valorCartasUsuario > valorCartasComputador){
-            console.log ("O usuário ganhou!")
-         }
-         else if (valorCartasUsuario < valorCartasComputador){
-            console.log ("O computador ganhou!")
-         }
-         else {
-            console.log ("Empate")
-         }
-          
-       }
+      const querMaisCartas = confirm(`Suas cartas são ${primeiraCartaUsuario.texto} ${segundaCartaUsuario.texto}. A carta revelada do computador é ${primeiraCartaComputador.texto}. \n Deseja comprar mais uma carta?`)
+      // for (; querMaisCartas === true ; )
 
 
-       
+
+      resultado(valorCartasUsuario, valorCartasComputador)
+     
+   
     }
+
     else {
-       console.log ("O jogo acabou.")
+      console.log ("O jogo acabou.")
     }
+
