2f4062c7d99ca264b8f64578d0d8324649f07f6e
Francisco Hugo de Azevedo Aguiar <hugo-phone@live.com>
2021-10-07 15:18:23 -0300

    Renderização condicional funcionando. Falta corrigir botão de excluir usuario

--- quinzena7/aula26-27/labenusers/src/App.js
+++ quinzena7/aula26-27/labenusers/src/App.js
@@ -3,6 +3,45 @@ import axios from 'axios';
 import React from 'react';
 import './App.css';
 
+const PaginaDeUsuarios= styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 10px;
+`
+
+const PaginaDeCadastro= styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 20px;
+`
+
+const FichaCadastro = styled.div`
+  border: 1px solid black;
+  width: 400px;
+  padding-bottom: 10px;
+  
+  
+  div{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 5px ;
+  }
+`
+
+const UsuarioCadastrado= styled.div`
+  border: 1px solid black;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 300px;
+  height: 50px;
+  padding: 10px;
+  
+`
+
 const headers = {
   headers: {
     Authorization: "francisco-aguiar-banu"
@@ -12,8 +51,13 @@ const headers = {
 export default class App extends React.Component {
   state= {
     inputNome: "",
-    inputEmail: ""
+    inputEmail: "",
+    usuarios: [],
+    paginaRenderizada: "cadastro"
+  }
 
+  componentDidMount() {
+    this.pegarListaDeUsuarios()
   }
 
 
@@ -26,26 +70,66 @@ export default class App extends React.Component {
     this.setState({inputEmail: e.target.value})
   }
 
+  onClickPaginaDeCadastro = () =>{
+    this.setState({paginaRenderizada:"usuarios"})
+  }
+
+  onClickPaginaDeUsuarios= () => {
+    this.setState({paginaRenderizada: "cadastro"})
+  }
+
+  paginaRenderizada = () => {
+    switch (this.state.paginaRenderizada){
+      case 'usuarios':
+        return this.paginaListaDeUsuarios()
+      default:
+        return this.paginaDeCadastro()
+    }
+  }
+
   paginaDeCadastro = () => {
     return (
-      <div>
-        <h3>Cadastro de usuário</h3>
-        <div>
-          <p>Nome do Usuário:</p>
-          <input
-            placeholder="Digite seu nome"
-            value= {this.state.inputNome}
-            onChange= {this.onChangeNome}/>
-        </div>
-        <div>
-          <p>Email:</p>
-          <input
-            placeholder="Digite seu email"
-            value= {this.state.inputEmail}
-            onChange= {this.onChangeEmail}/>
-        </div>
-        <button onClick={this.cadastrarUsuario}>Realizar Cadastro</button>
-      </div>
+      <PaginaDeCadastro>
+        <button onClick={this.onClickPaginaDeCadastro}>Página de Usuários</button>
+        <FichaCadastro>
+          <h3>Cadastro de usuário</h3>
+          
+          <div>
+            <p>Nome do Usuário:</p>
+            <input
+              placeholder="Digite seu nome"
+              value= {this.state.inputNome}
+              onChange= {this.onChangeNome}/>
+          </div>
+          <div>
+            <p>Email:</p>
+            <input
+              placeholder="Digite seu email"
+              value= {this.state.inputEmail}
+              onChange= {this.onChangeEmail}/>
+          </div>
+          <button onClick={this.cadastrarUsuario}>Realizar Cadastro</button>
+        </FichaCadastro>
+      </PaginaDeCadastro>
+    )
+  }
+
+  paginaListaDeUsuarios = () => {
+    return (
+      <PaginaDeUsuarios>
+        <button onClick={this.onClickPaginaDeUsuarios}>Página de Cadastro</button>
+        <h3>Lista de usuários cadastrados</h3>
+        
+        {this.state.usuarios.map((usuario) =>{
+          return (
+            <UsuarioCadastrado>
+              <li key={usuario.id}>{usuario.name}</li>
+              <button>Excluir</button>
+            </UsuarioCadastrado>
+          
+          )
+        })}
+      </PaginaDeUsuarios>
     )
   }
 
@@ -61,16 +145,39 @@ export default class App extends React.Component {
       .then((resultado) =>{
         alert("Cadastro realizado com sucesso!")
         this.setState({inputNome:"", inputEmail:""})
+        this.pegarListaDeUsuarios()
       })
       .catch((resultado)=> alert("Erro! Cadastro não realizado!"))
   }
 
+  pegarListaDeUsuarios = () => {
+    axios
+      .get("https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users", headers)
+      .then((resultado) => {
+        console.log(resultado.data)
+        this.setState({usuarios: resultado.data})
+      })
+      .catch((erro) => console.log(erro))
+  }
+
+  excluirUsuario = () => {
+    const header = {
+      headers: {
+        Authorization: "franciso-aguiar-banu"
+      }
+    }
+    axios
+      .delete()
+  }
+
 
   render() {
+
     return(
       <div className="App">
         <h1>Labenusers!</h1>
-        {this.paginaDeCadastro()}
+        {this.paginaRenderizada()}
+        
         
 
       </div>
