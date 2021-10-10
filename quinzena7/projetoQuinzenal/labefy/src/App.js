5ee8b584edb1e11406b8de182167d1cd479b755b
Francisco Hugo de Azevedo Aguiar <hugo-phone@live.com>
2021-10-10 17:00:10 -0300

    Margens do body ajustadas

--- quinzena7/projetoQuinzenal/labefy/src/App.js
+++ quinzena7/projetoQuinzenal/labefy/src/App.js
@@ -2,12 +2,31 @@ import React from "react";
 import styled from "styled-components";
 
 
+const ContainerPai = styled.div`
+  text-align: center;
+  
+`
+
+const Header = styled.header`
+  height: 100px;
+  background-color: #F0831E;
+  display: flex;
+  align-items: center;
+  padding-left: 20px;
+` 
+
 export class App extends React.Component {
   render() {
     return (
-      <div>
-        ola
-      </div>
+      <ContainerPai>
+        <Header>
+          <h1> 🎶 LabeFy Music</h1>
+        </Header>
+        <div>
+          oi
+        </div>
+        
+      </ContainerPai>
     );
   }
 }
--- quinzena7/projetoQuinzenal/labefy/src/index.js
+++ quinzena7/projetoQuinzenal/labefy/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Reset from './reset';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Reset/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
