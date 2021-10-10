ae31a45c7a260d7a92cbcadb352f3b9f11bfc4c6
Francisco Hugo de Azevedo Aguiar <hugo-phone@live.com>
2021-10-10 15:47:43 -0300

    Componente React criado

---
+++ quinzena7/projetoQuinzenal/labefy/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div>
+        ola
+      </div>
+    );
+  }
+}
+
+export default App;
