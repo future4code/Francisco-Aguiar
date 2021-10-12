033ded1522a45786cda5ec89aa45b97ad1b2dc27
Francisco Hugo de Azevedo Aguiar <hugo-phone@live.com>
2021-10-12 10:49:05 -0300

    Criado arquivo js para ser component das estilizações

--- quinzena7/aula26-27/labenusers/src/App.js
+++ quinzena7/aula26-27/labenusers/src/App.js
@@ -3,44 +3,12 @@ import axios from 'axios';
 import React from 'react';
 import './App.css';
 
-const PaginaDeUsuarios= styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  row-gap: 10px;
-`
+import {PaginaDeUsuarios} from './styled.js'
+import {PaginaDeCadastro} from './styled.js'
+import {FichaCadastro} from './styled.js'
+import {UsuarioCadastrado} from './styled.js'
 
-const PaginaDeCadastro= styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  row-gap: 20px;
-`
 
-const FichaCadastro = styled.div`
-  border: 1px solid black;
-  width: 400px;
-  padding-bottom: 10px;
-  
-  
-  div{
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 5px ;
-  }
-`
-
-const UsuarioCadastrado= styled.div`
-  border: 1px solid black;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 300px;
-  height: 50px;
-  padding: 10px;
-  
-`
 
 const headers = {
   headers: {
---
+++ quinzena7/aula26-27/labenusers/src/styled.js
@@ -0,0 +1,42 @@
+import styled from "styled-components"
+
+export const PaginaDeUsuarios= styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 10px;
+`
+
+export const PaginaDeCadastro= styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 20px;
+`
+
+export const FichaCadastro = styled.div`
+  border: 1px solid black;
+  width: 400px;
+  padding-bottom: 10px;
+  
+  
+  div{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 5px ;
+  }
+`
+
+export const UsuarioCadastrado= styled.div`
+  border: 1px solid black;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 300px;
+  height: 50px;
+  padding: 10px;
+  
+`
+
+
