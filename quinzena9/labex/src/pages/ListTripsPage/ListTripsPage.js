b8ea193d530ea48cfa4011106e12f82ef9a7a06e
Francisco Hugo de Azevedo Aguiar <hugo-phone@live.com>
2021-11-05 16:00:04 -0300

    Pagina ListTripsPage funcionando a requisição e mostrando a lista de viagens

--- quinzena9/labex/src/pages/ListTripsPage/ListTripsPage.js
+++ quinzena9/labex/src/pages/ListTripsPage/ListTripsPage.js
@@ -1,7 +1,21 @@
 import { useNavigate } from "react-router"
+import useRequestData from "../../hooks/useRequestData"
+import { UrlBase } from "../../constants/constants"
 
 const ListTripsPage = () =>{
     const navigate = useNavigate()
+    const [data, isLoading, error] = useRequestData("get", `${UrlBase}/trips`)
+    const listTrips = data.trips && data.trips.map((trip) => {
+        return(
+            <div key={trip.id}>
+                <p><b>Nome:</b> {trip.name}</p>
+                <p><b>Descrição:</b> {trip.description}</p>
+                <p><b>Planeta:</b> {trip.planet}</p>
+                <p><b>Duração:</b> {trip.durationInDays}</p>
+                <p><b>Data:</b> {trip.date}</p>                
+            </div>
+        )
+    })
 
     return(
         <>
@@ -13,7 +27,10 @@ const ListTripsPage = () =>{
             <main>
                 <h1>Viagens Disponíveis</h1>
                 <div>
-                    lista de viagens
+                    {isLoading && <p>Carregando...</p>}
+                    {!isLoading && error && <p>Ocorreu um erro!</p>}
+                    {!isLoading && data.trips && data.trips.length > 0 && listTrips}
+                    {!isLoading && data.trips && data.trips.length === 0 && (<p>Não há viagens disponíveis!</p>)}
                 </div>
             </main>
         </>
